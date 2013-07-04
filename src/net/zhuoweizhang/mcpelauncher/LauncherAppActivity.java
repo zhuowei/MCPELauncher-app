492818f95bbc62e2ee404be4830836903e644f0a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-07-04 16:06:05 -0700

    1.2

--- src/net/zhuoweizhang/mcpelauncher/LauncherAppActivity.java
+++ src/net/zhuoweizhang/mcpelauncher/LauncherAppActivity.java
@@ -1,7 +1,43 @@
 package net.zhuoweizhang.mcpelauncher;
 
-import android.app.Activity;
+import android.app.*;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.*;
+import android.widget.*;
+import com.google.ads.*;
 
 public class LauncherAppActivity extends LauncherActivity {
+
+	@Override
+	protected Intent getOptionsActivityIntent() {
+		return new Intent(this, MainMenuOptionsAppActivity.class);
+	}
+
+	@Override
+	public void onPrepareDialog(int dialogId, Dialog dialog) {
+		switch (dialogId) {
+			case DIALOG_RUNTIME_OPTIONS:
+				prepareRuntimeOptionsDialog(dialog);
+			default:
+				super.onPrepareDialog(dialogId, dialog);
+		}
+	}
+
+	protected void prepareRuntimeOptionsDialog(Dialog dialog) {
+		FrameLayout view = (FrameLayout) dialog.findViewById(android.R.id.custom);
+		view.setVisibility(View.VISIBLE);
+		ViewParent parent = view.getParent();
+		if (parent != null) ((View) parent).setVisibility(View.VISIBLE);
+
+		AdView adView = (AdView) view.findViewById(0xbeefbeef);
+		if (adView == null) {
+			adView = new AdView(this, AdSize.BANNER, "a151b8f9c5d33b5");
+			adView.setId(0xbeefbeef);
+			view.addView(adView);
+		}
+		AdRequest adRequest = new AdRequest();
+		adRequest.addTestDevice("DF28838C26BDFAE7EB063BFEB7A241D3");
+		adView.loadAd(adRequest);
+	}
 }
---
+++ src/net/zhuoweizhang/mcpelauncher/MainMenuOptionsAppActivity.java
@@ -0,0 +1,31 @@
+package net.zhuoweizhang.mcpelauncher;
+
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+import com.google.ads.*;
+
+public class MainMenuOptionsAppActivity extends MainMenuOptionsActivity {
+
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		try {
+			addAds();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addAds() {
+		ViewGroup view = (ViewGroup) getWindow().getDecorView();
+		LinearLayout content = (LinearLayout) view.getChildAt(0);
+
+		AdView adView = new AdView(this, AdSize.SMART_BANNER, "a151b8f9c5d33b5");
+		AdRequest adRequest = new AdRequest();
+		adRequest.addTestDevice("DF28838C26BDFAE7EB063BFEB7A241D3");
+		content.addView(adView, 0);
+		adView.loadAd(adRequest);
+	}
+
+}
