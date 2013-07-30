2f3151c86a7040f7c2e6538ab98bdd2ffba713f7
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-07-29 21:25:38 -0700

    Added advertisements after leaving a world

--- src/net/zhuoweizhang/mcpelauncher/LauncherAppActivity.java
+++ src/net/zhuoweizhang/mcpelauncher/LauncherAppActivity.java
@@ -7,7 +7,16 @@ import android.view.*;
 import android.widget.*;
 import com.google.ads.*;
 
-public class LauncherAppActivity extends LauncherActivity {
+public class LauncherAppActivity extends LauncherActivity implements AdListener {
+
+	private InterstitialAd interstitial;
+	private boolean needsShowAd = false;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		loadInterstitialAdvertisement();
+	}
 
 	@Override
 	protected Intent getOptionsActivityIntent() {
@@ -39,5 +48,72 @@ public class LauncherAppActivity extends LauncherActivity {
 		AdRequest adRequest = new AdRequest();
 		adRequest.addTestDevice("DF28838C26BDFAE7EB063BFEB7A241D3");
 		adView.loadAd(adRequest);
+	}
+
+	@Override
+	public void leaveGameCallback() {
+		this.runOnUiThread(new Runnable() {
+			public void run() {
+				showAdvertisement();
+			}
+		});
+	}
+
+	protected void loadInterstitialAdvertisement() {
+		if (interstitial == null) {
+			interstitial = new InterstitialAd(this, "a151b8f9c5d33b5");
+			interstitial.setAdListener(this);
+		}
+		AdRequest adRequest = new AdRequest();
+		adRequest.addTestDevice("DF28838C26BDFAE7EB063BFEB7A241D3");
+		interstitial.loadAd(adRequest);
+	}
+
+	public void showAdvertisement() {
+		if (interstitial.isReady()) {
+			interstitial.show();
+			loadInterstitialAdvertisement();
+			needsShowAd = false;
+		} else {
+			needsShowAd = true;
+		}
+	}
+
+	/** Called when an ad is clicked and about to return to the application. */
+	@Override
+	public void onDismissScreen(Ad ad) {
+
+	}
+
+	/** Called when an ad was not received. */
+	@Override
+	public void onFailedToReceiveAd(Ad ad, AdRequest.ErrorCode error) {
+	}
+
+	/**
+	 * Called when an ad is clicked and going to start a new Activity that will
+	 * leave the application (e.g. breaking out to the Browser or Maps
+	 * application).
+	 */
+	@Override
+	public void onLeaveApplication(Ad ad) {
+	}
+
+	/**
+	 * Called when an Activity is created in front of the app (e.g. an
+	 * interstitial is shown, or an ad is clicked and launches a new Activity).
+	 */
+	@Override
+	public void onPresentScreen(Ad ad) {
+	}
+
+	/** Called when an ad is received. */
+	@Override
+	public void onReceiveAd(Ad ad) {
+		if (ad == interstitial && needsShowAd) {
+			interstitial.show();
+			loadInterstitialAdvertisement();
+			needsShowAd = false;
+		}
 	}
 }
